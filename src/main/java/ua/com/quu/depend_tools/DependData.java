508041c751bb36fa88502cc88236755582d321a5
yarrtuur <sashagmaster@gmail.com>
2018-07-11 16:25:49 +0300

    first step to depende data

--- src/main/java/ua/com/quu/depend_tools/DependData.java
+++ src/main/java/ua/com/quu/depend_tools/DependData.java
@@ -1,9 +1,20 @@
 package ua.com.quu.depend_tools;
 
 import ua.com.quu.data_source.DataContainer;
+import ua.com.quu.data_source.UnitTypesImpl;
+
+import java.util.List;
 
 public class DependData {
+    private List<UnitTypesImpl> waitingUnitList;
+    private List<UnitTypesImpl> queryUnitList;
+
     public DependData(DataContainer dataContainer) {
+        waitingUnitList = dataContainer.getWaitingUnitList();
+        queryUnitList = dataContainer.getQueryUnitList();
+
     }
+
+
 
 }
