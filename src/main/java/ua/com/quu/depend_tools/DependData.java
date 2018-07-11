9ce1e5e44bac310adc028ee5e069f9a37173c5d2
yarrtuur <sashagmaster@gmail.com>
2018-07-11 17:05:26 +0300

    first step to depende data2

--- src/main/java/ua/com/quu/depend_tools/DependData.java
+++ src/main/java/ua/com/quu/depend_tools/DependData.java
@@ -8,13 +8,24 @@ import java.util.List;
 public class DependData {
     private List<UnitTypesImpl> waitingUnitList;
     private List<UnitTypesImpl> queryUnitList;
+//C 1.1 8.15.1 P 15.10.2012 83
+//C 1 10.1 P 01.12.2012 65C 1.1 5.5.1 P 01.11.2012 117
+//D 1.1 8 P 01.01.2012-01.12.2012
+//C 3 10.2 N 02.10.2012 100
+//D 1 * P 8.10.2012-20.11.2012
+//D 3 10 P 01.12.2012
+//C service_id[.variation_id] question_type_id[.category_id.[sub-category_id]] P/N date time
+//D service_id[.variation_id] question_type_id[.category_id.[sub-category_id]] P/N date_from[-date_to]
 
     public DependData(DataContainer dataContainer) {
         waitingUnitList = dataContainer.getWaitingUnitList();
         queryUnitList = dataContainer.getQueryUnitList();
-
+        findDependences();
     }
 
+    private void findDependences() {
+
+    }
 
 
 }
